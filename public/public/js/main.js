ac7fd406b3f60c16bb1d02885e760b9861a41bd2
Rie Kimura <92915220+rie-kimura@users.noreply.github.com>
2022-09-22 16:25:14 +0900

    slideToggleの追加

---
+++ public/public/js/main.js
@@ -0,0 +1,78 @@
+// -----ページ内リンク----- //
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
+
+// -----ページトップリンク----- //
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+
+// -----ヘッダー上部固定----- //
+//スクロールすると上部に固定させるための設定を関数でまとめる
+function FixedAnime() {
+	var headerH = $('#header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//headerの高さ以上になったら
+			$('#header').addClass('fixed');//fixedというクラス名を付与
+		}else{//それ以外は
+			$('#header').removeClass('fixed');//fixedというクラス名を除去
+		}
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+})
+
+
+// -----sliedToggle----- //
+$(function(){
+  $("#employee1").click(function () {
+    $('.description_employee_upper').slideToggle();
+  });
+ 
+  $("#employee2").click(function () {
+    $('.description_employee_under').slideToggle();
+  });
+ 
+  $("#employee3").click(function () {
+    $('.description_employee_under').slideToggle();
+  });
+	$("#employee4").click(function () {
+    $('.description_employee_under').slideToggle();
+  });
+});
